0f5a46631edec9a58e828ae8bca128dd7bf1c171
Meellaa <marcelaiftimiaa@gmail.com>
2024-07-25 11:43:04 +0300

    Conectare butoane pentru favorite , shop si shelter cart

---
+++ Shop/favproducts.js
@@ -0,0 +1,629 @@
+let favProducts= [
+    {
+       id:1,
+       name: "Jucarie ros",
+       image: "/Shop/pozemagazin/download-1.jpg",
+       price: "15",
+   },
+   {
+       id:2,
+       name: "JDevora",
+       image:"/Shop/pozemagazin/download.jpg",
+       price: "95 ",
+   },
+   {
+       id:3,
+       name: "Jucarie mancare",
+       image: "/Shop/pozemagazin/jucarie.jpeg",
+       price: "55",
+   },
+   {
+       id:4,
+       name: "Devora asta",
+       image: "/Shop/pozemagazin/devora.jpeg",
+       price: "15",
+   },
+   {
+       id:5,
+       name: "Jucarie ro",
+       image: "/Shop/pozemagazin/download-1.jpg",
+       price: "115",
+   },
+   {
+       id:6,
+       name: "JDevora",
+       image: "/Shop/pozemagazin/download.jpg",
+       price: "5",
+   }  
+]
+let hranaProducts=[
+   {
+       id:21,
+       name: "HJucarie ros",
+       image: "/Shop/pozemagazin/download-1.jpg",
+       price: "15",
+   },
+   {
+       id:22,
+       name: "HJDevora",
+       image:"/Shop/pozemagazin/download.jpg",
+       price: "95 ",
+   },
+   {
+       id:23,
+       name: "HJucarie mancare",
+       image: "/Shop/pozemagazin/jucarie.jpeg",
+       price: "55",
+   },
+   {
+       id:24,
+       name: "HDevora asta",
+       image: "/Shop/pozemagazin/devora.jpeg",
+       price: "15",
+   },
+   {
+       id:25,
+       name: "HJucarie ro",
+       image: "/Shop/pozemagazin/download-1.jpg",
+       price: "115",
+   },
+   {
+       id:26,
+       name: "HJDevora",
+       image: "/Shop/pozemagazin/download.jpg",
+       price: "5",
+   }
+]
+let dietaVeterinaraProducts= [
+   {
+      id:31,
+      name: "Jucarie ros",
+      image: "/Shop/pozemagazin/download-1.jpg",
+      price: "15",
+  },
+  {
+      id:32,
+      name: "JDevora",
+      image:"/Shop/pozemagazin/download.jpg",
+      price: "95 ",
+  },
+  {
+      id:33,
+      name: "Jucarie mancare",
+      image: "/Shop/pozemagazin/jucarie.jpeg",
+      price: "55",
+  },
+  {
+      id:34,
+      name: "Devora asta",
+      image: "/Shop/pozemagazin/devora.jpeg",
+      price: "15",
+  },
+  {
+      id:35,
+      name: "Jucarie ro",
+      image: "/Shop/pozemagazin/download-1.jpg",
+      price: "115",
+  },
+  {
+      id:36,
+      name: "JDevora",
+      image: "/Shop/pozemagazin/download.jpg",
+      price: "5",
+  }  
+]
+let recompenseProducts=[
+  {
+      id:41,
+      name: "HJucarie ros",
+      image: "/Shop/pozemagazin/download-1.jpg",
+      price: "15",
+  },
+  {
+      id:42,
+      name: "HJDevora",
+      image:"/Shop/pozemagazin/download.jpg",
+      price: "95 ",
+  },
+  {
+      id:43,
+      name: "HJucarie mancare",
+      image: "/Shop/pozemagazin/jucarie.jpeg",
+      price: "55",
+  },
+  {
+      id:44,
+      name: "HDevora asta",
+      image: "/Shop/pozemagazin/devora.jpeg",
+      price: "15",
+  },
+  {
+      id:45,
+      name: "HJucarie ro",
+      image: "/Shop/pozemagazin/download-1.jpg",
+      price: "115",
+  },
+  {
+      id:46,
+      name: "HJDevora",
+      image: "/Shop/pozemagazin/download.jpg",
+      price: "5",
+  }
+]
+let ingrijireProducts= [
+   {
+      id:51,
+      name: "Jucarie ros",
+      image: "/Shop/pozemagazin/download-1.jpg",
+      price: "15",
+  },
+  {
+      id:52,
+      name: "JDevora",
+      image:"/Shop/pozemagazin/download.jpg",
+      price: "95 ",
+  },
+  {
+      id:53,
+      name: "Jucarie mancare",
+      image: "/Shop/pozemagazin/jucarie.jpeg",
+      price: "55",
+  },
+  {
+      id:54,
+      name: "Devora asta",
+      image: "/Shop/pozemagazin/devora.jpeg",
+      price: "15",
+  },
+  {
+      id:55,
+      name: "Jucarie ro",
+      image: "/Shop/pozemagazin/download-1.jpg",
+      price: "115",
+  },
+  {
+      id:56,
+      name: "JDevora",
+      image: "/Shop/pozemagazin/download.jpg",
+      price: "5",
+  }  
+]
+let kiturProducts=[
+  {
+      id:61,
+      name: "HJucarie ros",
+      image: "/Shop/pozemagazin/download-1.jpg",
+      price: "15",
+  },
+  {
+      id:62,
+      name: "HJDevora",
+      image:"/Shop/pozemagazin/download.jpg",
+      price: "95 ",
+  },
+  {
+      id:63,
+      name: "HJucarie mancare",
+      image: "/Shop/pozemagazin/jucarie.jpeg",
+      price: "55",
+  },
+  {
+      id:64,
+      name: "HDevora asta",
+      image: "/Shop/pozemagazin/devora.jpeg",
+      price: "15",
+  },
+  {
+      id:65,
+      name: "HJucarie ro",
+      image: "/Shop/pozemagazin/download-1.jpg",
+      price: "115",
+  },
+  {
+      id:66,
+      name: "HJDevora",
+      image: "/Shop/pozemagazin/download.jpg",
+      price: "5",
+  }
+]
+let produseRomanestiProducts= [
+  {
+     id:71,
+     name: "Jucarie ros",
+     image: "/Shop/pozemagazin/download-1.jpg",
+     price: "15",
+ },
+ {
+     id:72,
+     name: "JDevora",
+     image:"/Shop/pozemagazin/download.jpg",
+     price: "95 ",
+ },
+ {
+     id:73,
+     name: "Jucarie mancare",
+     image: "/Shop/pozemagazin/jucarie.jpeg",
+     price: "55",
+ },
+ {
+     id:74,
+     name: "Devora asta",
+     image: "/Shop/pozemagazin/devora.jpeg",
+     price: "15",
+ },
+ {
+     id:75,
+     name: "Jucarie ro",
+     image: "/Shop/pozemagazin/download-1.jpg",
+     price: "115",
+ },
+ {
+     id:76,
+     name: "JDevora",
+     image: "/Shop/pozemagazin/download.jpg",
+     price: "5",
+ }  
+]
+
+const allProducts ={
+   fav:favProducts,
+   hrana:hranaProducts,
+   dietaVeterinara:dietaVeterinaraProducts,
+   recompense:recompenseProducts,
+   ingrijire:ingrijireProducts,
+   kituri:kiturProducts,
+   produseRomanesti:produseRomanestiProducts
+};
+
+const body = document.querySelector("body"),
+    produse = document.querySelector(".lists"),
+    favoriteCart =document.querySelector(".favoriteCart"),
+    donationCart =document.querySelector(".donationCart"),
+    shoppingCart = document.querySelector(".shoppingCart"),
+    closeCart = document.querySelectorAll(".close"),
+    favoriteProducts = document.querySelector(".favoriteProducts"),
+    shelterProducts =document.querySelector(".shelterProducts"),
+    cartProducts = document.querySelector(".cartProducts"),
+    shelterQuantity =document.querySelector(".shelterQuantity"),
+    shopQuantity = document.querySelector (".shopQuantity"),
+    totalShelter = document.querySelector (".totalShelter"),
+    totalShop = document.querySelector (".totalShop");
+   
+const favorite = document.getElementById('favorite'),
+    hrana = document.getElementById('hrana'),
+    kituri = document.getElementById('kituri'),
+    produseRomanesti = document.getElementById('produseRomanesti'),
+    recompense = document.getElementById('recompense'),
+    dietaVeterinara = document.getElementById('dietaVeterinara'),
+    ingrijire =document.getElementById('ingrijire');
+   
+const lista = document.getElementById('produse');
+const buton = document.getElementById('pro');
+const htmlInitial = buton.innerHTML;
+const submenuList = document.querySelectorAll('.submenu');
+    
+let cosDeCumparaturi = {};
+let cosAsociatie = {};
+let produseFavorite = []
+
+favoriteCart.onclick = () => {
+    body.classList.add("acti");
+    body.classList.remove("activ");
+    body.classList.remove("active");
+ };
+
+donationCart.onclick = () => {
+   body.classList.add("active");
+   body.classList.remove("activ");
+   body.classList.remove("acti");
+};
+
+shoppingCart.onclick = () => {
+   body.classList.add("activ");
+   body.classList.remove("active");
+   body.classList.remove("acti");
+};
+closeCart.forEach(button => {
+   button.onclick = () => {
+       body.classList.remove("active");
+       body.classList.remove("activ");
+       body.classList.remove("acti");
+   };
+});
+
+function pProducts(products, container, productType) {
+   container.innerHTML = "";
+   products.forEach((item) => {
+       let li = document.createElement("li");
+       li.classList.add("boxs");
+       li.innerHTML = `
+       <div class="icons">
+           <button onclick="addToFavorite('${productType}', ${item.id})"><i class="fa-solid fa-heart" style="color:#cedfc3;"></i></button>
+           <button onclick="addToShelter('${productType}', ${item.id})"><i class="fa-solid fa-shield-dog" style="color:#cedfc3;"></i></button>
+           <button onclick="addToCart('${productType}', ${item.id})"><i class="fa-solid fa-basket-shopping" style="color:#cedfc3;"></i></button>
+       </div>
+       <div class="box">
+           <img src="${item.image}" alt="${item.name}">
+           <h3>${item.name}</h3>
+           <p>${item.price} lei </p>
+       </div>`;
+       container.appendChild(li);
+   });
+}
+pProducts(favProducts, produse, 'fav');
+
+function addToCart(type, Id){
+   let allProduct = allProducts[type];
+   let product = allProduct.find(p => p.id === Id);
+ 
+   if (cosDeCumparaturi[Id] == null){
+       cosDeCumparaturi[Id] = {...product, shopQuantity:1};
+       cosDeCumparaturi[Id].shopQuantity= 1;
+       Toastify({
+        text: "Produsul a fost adăugat cu succes în COȘUL DE CUMPĂRĂTURI.",
+        duration: 1000, // durata în milisecunde
+        close: true,
+        gravity: "top", // `top` sau `bottom`
+        position: "right", // `left`, `center` sau `right`
+        backgroundColor: "linear-gradient(to right, rgb(118, 145, 118), rgb(113, 52, 99))",
+        color: "#cedfc3",
+        borderRadius: "7px",
+    }).showToast();
+      }else{
+       cosDeCumparaturi[Id].shopQuantity += 1;
+   }
+   reloadShopCart();
+}
+function addToShelter(type, Id){
+    let allProduc = allProducts[type];
+    let product = allProduc.find(p => p.id === Id);    
+
+if (!cosAsociatie[Id] ){
+    cosAsociatie[Id] = {...product, shelterQuantity: 1};
+    Toastify({
+        text: "Produsul a fost adăugat cu succes în CUMPĂRĂ PENTRU UN ADĂPOST.",
+        duration: 1000, // durata în milisecunde
+        close: true,
+        gravity: "top", // `top` sau `bottom`
+        position: "right", // `left`, `center` sau `right`
+        backgroundColor: "linear-gradient(to right, rgb(118, 145, 118), rgb(113, 52, 99))",
+        color: "#cedfc3",
+        borderRadius: "7px",
+    }).showToast();
+} else {
+    cosAsociatie[Id].shelterQuantity += 1;
+}
+reloadShelterCart();
+}
+function addToFavorite(type, Id) {
+    let allProduct = allProducts[type];
+    let product = allProduct.find(p => p.id === Id);
+ 
+     let favoriteIndex = produseFavorite.findIndex(fav => fav.id === Id && fav.type === type);
+ 
+     if (favoriteIndex === -1) {
+         produseFavorite.push({ ...product, type: type });
+         Toastify({
+            text: "Produsul a fost adăugat cu succes în FAVORITE.",
+            duration: 2000, // durata în milisecunde
+            close: true,
+            gravity: "top", // `top` sau `bottom`
+            position: "right", // `left`, `center` sau `right`
+            backgroundColor: "linear-gradient(to right, rgb(118, 145, 118), rgb(113, 52, 99))",
+            color: "#cedfc3",
+            borderRadius: "7px",
+        }).showToast();
+        } else {
+         produseFavorite.splice(favoriteIndex, 1);
+        console.log("cos");
+    }
+    reloadFavoriteCart();
+}
+function addToCartFromFavorite(Id){
+    let productIndex = produseFavorite.findIndex(p => p.id === Id);
+    if (productIndex !== -1){
+        let product = produseFavorite[productIndex];
+        if (!cosDeCumparaturi[Id]){
+            cosDeCumparaturi[Id] = { ...product, shopQuantity:1 };
+        }else{
+            cosDeCumparaturi[Id].shopQuantity += 1;
+        }
+        produseFavorite.splice(productIndex, 1);
+        reloadShopCart();
+        reloadFavoriteCart();
+    }
+}
+function addToShelterFromFavorite(Id){
+    let productIndex = produseFavorite.findIndex(p => p.id === Id);
+    if (productIndex !== -1){
+    let product =produseFavorite[productIndex];
+        if(!cosAsociatie[Id] ){
+            cosAsociatie[Id] = { ...product, shelterQuantity:1 };
+        }else{
+            cosAsociatie[Id].shelterQuantity += 1;
+        }
+        produseFavorite.splice(productIndex, 1);
+        reloadShelterCart();
+        reloadFavoriteCart();
+    }
+}
+function removeFromFavorite(Id){
+    console.log(`Removing product with ID: ${Id}`);
+    const productIndex = produseFavorite.findIndex(p => p.id === Id);
+    if (productIndex !== -1){
+        produseFavorite.splice(productIndex, 1);
+        reloadFavoriteCart();
+    }
+}
+function changeQuantityShop(key, quantity){
+    if(quantity == 0){
+        delete cosDeCumparaturi[key];
+    }else{
+        cosDeCumparaturi[key].shopQuantity = quantity;
+    }
+    reloadShopCart();
+ }
+ function changeQuantityShelter(key, quantity){
+        if(quantity == 0){
+            delete cosAsociatie[key];
+        }else{
+            cosAsociatie[key].shelterQuantity = quantity;
+        }
+        reloadShelterCart();
+ }
+
+
+function reloadFavoriteCart(){
+    favoriteProducts.innerHTML ='';
+    produseFavorite.forEach((item) => {
+    let li = document.createElement("li");
+    li.innerHTML = `
+        <img src ="${item.image}">
+       <div> ${item.name}</div>
+       <div class = "butons">
+       <button class = "cart" onclick="addToCartFromFavorite(${item.id})">Adaugă în coș</button>
+       <button class = "cart" onclick="addToShelterFromFavorite(${item.id})">Adaugă in DONATII</button>
+       <button onclick ="removeFromFavorite(${item.id})">Sterge </button>
+       </div>`
+
+       favoriteProducts.appendChild(li);
+});
+}
+function reloadShopCart(){
+    cartProducts.innerHTML="";
+    let count = 0;
+    let totalShopPrice = 0; 
+ 
+    Object.values(cosDeCumparaturi).forEach((item) =>{
+        totalShopPrice += parseInt (item.price*item.shopQuantity);
+        count += item.shopQuantity;
+        let li = document.createElement("li");
+        li.innerHTML = `
+        <img src ="${item.image}">
+        <div> ${item.name}</div>
+        <div> ${item.price} lei </div>
+        <div class="but">
+        <button onclick = "changeQuantityShop(${item.id}, ${item.shopQuantity-1})"> - </button>
+        <div> ${item.shopQuantity}</div>
+        <button onclick = "changeQuantityShop(${item.id}, ${item.shopQuantity+1})"> + </button>
+        </div>`
+        cartProducts.appendChild(li);
+    })
+ 
+    totalShop.innerHTML = `Subtotal : ${totalShopPrice} lei (${count} produse) `;
+    shopQuantity.innerHTML = count;
+ }
+function reloadShelterCart(){
+   shelterProducts.innerHTML ="";
+   let countS = 0;
+   let totalShelterPrice = 0;
+
+   Object.values(cosAsociatie).forEach((item) =>{
+       totalShelterPrice += parseInt(item.price*item.shelterQuantity);
+       countS += item.shelterQuantity;
+       let li = document.createElement("li");
+       li.innerHTML = `
+       <img src ="${item.image}">
+       <div> ${item.name}</div>
+       <div> ${item.price} lei </div>
+       <div class= "but">
+       <button onclick = "changeQuantityShelter(${item.id}, ${item.shelterQuantity - 1})">- </button>
+       <div> ${item.shelterQuantity} </div>
+       <button onclick = "changeQuantityShelter(${item.id}, ${item.shelterQuantity + 1})"> + </button>
+       </div>`
+
+       shelterProducts.appendChild(li);
+   })
+   totalShelter.innerHTML = `Subtotal : ${totalShelterPrice} lei (${countS} produse) `;
+   shelterQuantity.innerHTML = countS;
+}
+
+
+function afiseazaDiv(id) {
+    console.log(`Afișez div-ul: ${id}`);
+    const divuri = ['recompense','ingrijire', 'hrana', 'kituri', 'produseRomanesti','dietaVeterinara'];
+
+    divuri.forEach(function(divID) { 
+        const div = document.getElementById(divID);
+       if (div) {
+         div.style.display = 'none';
+       }
+        }); 
+
+    const divSelectat = document.getElementById(id);
+        if (divSelectat){
+            divSelectat.style.display = "block";     
+            favorite.style.display = "none";
+           
+            const ul =divSelectat.querySelector('ul.lists');
+            if(ul){
+                const productType = id ;
+                const products =allProducts[productType];
+                if (products){
+                    pProducts(products, ul, productType);
+                }else{
+                    console.log("nici un produs", productType);
+                }}
+                
+        } else {
+            console.log("Div-ul nu a fost găsit:", id);
+        }
+}
+
+
+document.addEventListener ('DOMContentLoaded', function(){
+    const recompenseli =document.getElementById('recompenseLi');
+    const dietaVeterinarali =document.getElementById('dietaVeterinaraLi');
+    const hranali =document.getElementById('hranaLi');
+    const ingrijireli =document.getElementById('ingrijireLi');
+    const kiturili =document.getElementById('kituriLi');
+    const produseRomanestili =document.getElementById('produseRomanestiLi');
+    
+    ingrijireli.addEventListener("click", function() {
+        afiseazaDiv('ingrijire');
+    });
+    recompenseli.addEventListener("click", function() {
+        afiseazaDiv('recompense');
+    });
+    hranali.addEventListener("click", function() {
+        afiseazaDiv('hrana');
+    });
+    kiturili.addEventListener("click", function() {
+        afiseazaDiv('kituri');
+    });
+    produseRomanestili.addEventListener("click", function() {
+        afiseazaDiv('produseRomanesti');
+    });
+    dietaVeterinarali.addEventListener("click", function() {
+        afiseazaDiv('dietaVeterinara');
+    });
+});
+
+
+buton.addEventListener('click' , function(e){
+    e.preventDefault();
+if (lista.style.display === 'none'){
+lista.style.display = 'inline-block';
+buton.textContent = "Inchide Meniul";
+}
+else{lista.style.display='none';
+buton.innerHTML= htmlInitial;
+}
+});
+
+
+submenuList.forEach(submenu => {
+    const subsubmenu = submenu.querySelector('.subsubmenu'); 
+    
+submenu.addEventListener('mouseenter', function(){
+    if(subsubmenu.style.display ==='none') {
+        subsubmenu.style.display = 'grid';
+      console.log('Mouse-ul a atins secțiunea:');
+    }
+    else{subsubmenu.style.display ='none';
+        }
+});
+submenu.addEventListener('mouseleave', function() {
+    subsubmenu.style.display = 'none';
+});
+});
